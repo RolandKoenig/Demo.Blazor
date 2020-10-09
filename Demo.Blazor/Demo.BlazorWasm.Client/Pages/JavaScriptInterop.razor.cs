8dbf883605a762580f2e1864211b3d397e0517a2
Roland König <roland.koenig@rolandk.de>
2020-10-09 09:13:03 +0200

    Added simple javascript interop

---
+++ Demo.Blazor/Demo.BlazorWasm.Client/Pages/JavaScriptInterop.razor.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Demo.BlazorWasm.Client.Pages
+{
+    public partial class JavaScriptInterop
+    {
+        [Inject]
+        public IJSRuntime JSRuntime
+        {
+            get;
+            set;
+        }
+
+        public async Task CallJavaScript_AlertAsync()
+        {
+            await this.JSRuntime.InvokeVoidAsync("alert", "This is a test message");
+        }
+    }
+}
